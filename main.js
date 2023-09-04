5e11e991e2d2f87401d6798ad99dfe778ed05435
Kwasi O-T <tweneboah21@gmail.com>
2023-09-04 17:46:40 +0000

    added scripts folder to assets

--- main.js
+++ main.js
@@ -145,3 +145,19 @@ const config = {
     particlesJS("background", config);
     };
     
+
+    // form management
+
+    // let contactForm = document.getElementById("contactForm");
+
+    // contactForm.addEventListener("submit", (e) => {
+    //     e.preventDefault();
+
+    //     let name = document.getElementById("fname");
+
+    //     if(name.value == "") {
+    //         alert("Please enter your name");
+    //     } else {
+    //         alert(`${name.value}, the form was submitted successfully.\n Thank you for reaching out to me!`);
+    //     }
+    // });
