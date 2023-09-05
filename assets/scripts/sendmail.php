b014653e30eb838a9444c5eb83ba343b176b90b6
Kwasi O-T <tweneboah21@gmail.com>
2023-09-05 00:27:26 +0000

    removed page redirect from php file

--- assets/scripts/sendmail.php
+++ assets/scripts/sendmail.php
@@ -1,26 +1,19 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(
-        !empty($_POST['name'])
-        && !empty($_POST['email'])
-        && !empty($_POST['message'])
-    ){
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $message = $_POST["message"];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(!empty($_POST['name']) && !empty($_POST['email'])&& !empty($_POST['message'])
+        ){
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+            $message = $_POST["message"];
 
 
-        $to = "contact@iamkwasi.dev";
-        $subject = "New Contact Form Submission";
-        $body = "Name: {$name}\nEmail: {$email}\nMessage: {$message}";
-        $headers = "From: {$email}";
+            $to = "contact@iamkwasi.dev";
+            $subject = "New Contact Form Submission";
+            $body = "Name: {$name}\nEmail: {$email}\nMessage: {$message}";
+            $headers = "From: {$email}";
 
 
-        mail($to, $subject, $body, $headers);
-
-        // Redirect the user back to the same page
-        header("Location: ".$_SERVER['PHP_SELF']);
-        exit();
-    }
-}    
+            mail($to, $subject, $body, $headers);
+        }
+    }  
 ?>
