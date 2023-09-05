6f6ae8e273e5581cea6ae1bdbbc27d1b49664968
Kwasi O-T <tweneboah21@gmail.com>
2023-09-05 02:53:32 +0000

    added alert and redirection to php file

--- assets/scripts/sendmail.php
+++ assets/scripts/sendmail.php
@@ -1,4 +1,9 @@
 <?php
+    
+    // Redirect the user back to the same page
+    header("Location: ../index.html", true, 301);
+    exit();
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if(!empty($_POST['name']) && !empty($_POST['email'])&& !empty($_POST['message'])
         ){
@@ -13,7 +18,26 @@
             $headers = "From: {$email}";
 
 
-            mail($to, $subject, $body, $headers);
+            if(mail($to, $subject, $body, $headers)) {
+                function_success("Your message has been sent successfully. Thank you!");
+                function function_success($msg) {
+                echo "<script type='text/javascript'>alert('$msg');</script>";
+            }
+            } else {
+                function_failed("Sorry, message submission failed. Please try again");
+                function function_failed($msg) {
+                echo "<script type='text/javascript'>alert('$msg');</script>";
+            }
+            }
+
+            
         }
-    }  
+    } 
+    
+
+    "<script type='text/javascript'>
+        setTimeout(function () {
+            window.location.href = 'https://iamkwasi.dev';         
+        },5000); // 5 seconds
+    </script>"
 ?>
